6c3c79d4a982ee1fad2f282fe2c8535e14c3de63
shakarashvili <shaqarashvili25@gmail.com>
2024-06-23 13:14:17 +0400

    Third commit

--- src/test/java/Test/TestRunner.java
+++ src/test/java/Test/TestRunner.java
@@ -34,39 +34,40 @@ static TestData testData;
 }
 @Test
 @Feature("ნეგატიური ქეისი ქართული ენისთვის, გადავცემთ არარსებული მომხმარებლის სახელს")
-    public static void Test1() throws InterruptedException {
-        TestData Maindata = new TestData(driver);
-        Maindata.RunAuthorizationPage();
-        Maindata.ChooseKartvelianLanguage();
-        Maindata.SingUpGeo();
+    public void Test1() throws InterruptedException {
+
+    testData.RunAuthorizationPage();
+    testData.ChooseKartvelianLanguage();
+    testData.SingUpGeo();
+
 }
 
     @Test
     @Feature("ნეგატიური ქეისი მეგრული ენისთვის, მომხმარებლის სახელს გადავცემთ ციფრებთან ერთად")
-    public static void Test2() throws InterruptedException {
-        TestData Maindata = new TestData(driver);
-        Maindata.RunAuthorizationPage();
-        Maindata.ChooseMegrelianLanguage();
-        Maindata.SingUpMeg();
+    public  void Test2() throws InterruptedException {
+
+        testData.RunAuthorizationPage();
+        testData.ChooseMegrelianLanguage();
+        testData.SingUpMeg();
     }
 
     @Test
     @Feature("ნეგატიური ქეისი სვანური ენისთვის, პაროლის ველში ვიყენებთ სფეისებს")
-    public static void Test3() throws InterruptedException {
-        TestData Maindata = new TestData(driver);
-        Maindata.RunAuthorizationPage();
-        Maindata.ChooseSvanLanguage();
-        Maindata.SingUpSvan();
+    public  void Test3() throws InterruptedException {
+
+        testData.RunAuthorizationPage();
+        testData.ChooseSvanLanguage();
+        testData.SingUpSvan();
     }
 
     @Test(dataProvider = "loginParameters" )
     @Feature("ტესტში გამოყენებულია დატა პროვაიდერი ")
     public  void Test4(String name , String password) throws InterruptedException {
-        TestData Maindata = new TestData(driver);
-        Maindata.RunAuthorizationPage();
-        Maindata.ChooseKartvelianLanguage();
-        Maindata.SingUpGeo2(name,password);
-       // Maindata.AuthorizationButton.click();
+
+        testData.RunAuthorizationPage();
+        testData.ChooseKartvelianLanguage();
+        testData.SingUpGeo2(name,password);
+
     }
     @DataProvider(name= "loginParameters")
     public Object[][] loginParameters(){
@@ -81,11 +82,10 @@ static TestData testData;
     @Feature("რენდომ ფუნქციით ტესტირება")
     public void Test5() throws InterruptedException {
         SoftAssert softAssert = new SoftAssert();
-        TestData Maindata = new TestData(driver);
-        Maindata.RunAuthorizationPage();
-        Maindata.ChooseMegrelianLanguage();
-        Maindata.Randomizer();
-        softAssert.assertTrue(Maindata.InvalidMessage.isDisplayed());
+        testData.RunAuthorizationPage();
+        testData.ChooseMegrelianLanguage();
+        testData.Randomizer();
+        softAssert.assertTrue(testData.InvalidMessage.isDisplayed());
         softAssert.assertAll();
 
     }
